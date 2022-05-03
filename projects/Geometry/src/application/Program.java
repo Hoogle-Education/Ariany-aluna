e6d96d4d16fad801d5a5fd39057016b41dfe30ff
Hugo Rafael <hugorafell@gmail.com>
2022-05-03 18:03:47 -0300

    update aula 03/05/22

---
+++ projects/Geometry/src/application/Program.java
@@ -0,0 +1,15 @@
+package application;
+
+import entities.Quadrilatero;
+import entities.enums.Color;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Quadrilatero quad1 = new Quadrilatero(Color.BLUE, 12);
+		Quadrilatero quad2 = new Quadrilatero(Color.RED, 1, 2, 3, 4);
+		
+	}
+
+}
